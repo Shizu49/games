50adc327fd59a4ff33c5903b9174a4eb041227f8
Momato <165156210+Shizu49@users.noreply.github.com>
2025-03-19 22:03:16 +0900

    Update yugo_puzzle1.js

--- yugo_puzzle1.js
+++ yugo_puzzle1.js
@@ -6,6 +6,11 @@ document.addEventListener("click", (event) => {
         target.closest("details")?.removeAttribute("open");
     }
 });
+document.addEventListener("toggle", (event) => {
+    if (event.target.tagName === "DETAILS") {
+        event.target.dataset.visited = "true";
+    }
+});
 // thumbクリックでYoutube読込
 document.querySelectorAll(".thumb").forEach(thumb => 
     thumb.addEventListener("click", () => {
