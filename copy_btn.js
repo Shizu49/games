5ec45ea538b8a3dbdae7b293bbee1f564d740b1a
Momato <165156210+Shizu49@users.noreply.github.com>
2025-04-04 22:45:42 +0900

    Update copy_btn.js

--- copy_btn.js
+++ copy_btn.js
@@ -1,12 +1,14 @@
-  function copyButton(button) {
-      const textElement = button.querySelector(&quot;p&quot;);
-      const codeElement = button.closest(&quot;pre&quot;).querySelector(&quot;code&quot;);
-      if (!codeElement) {
-          textElement.innerText = &quot;Failed&quot;;
-      } else {
-          navigator.clipboard.writeText(codeElement.textContent.trim()).then(() =&gt; {
-              textElement.innerText = &quot;Copied !&quot;;
-          });
-      }
-      setTimeout(() =&gt; textElement.innerText = &quot; &quot;, 2000);
-  }
+window.onload = function() {
+    window.copyButton = function(button) {
+        const textElement = button.querySelector("p");
+        const codeElement = button.closest("pre")?.querySelector("code");
+        if (!codeElement) {
+            textElement.innerText = "Failed";
+        } else {
+            navigator.clipboard.writeText(codeElement.textContent.trim()).then(() => {
+                textElement.innerText = "Copied !";
+            });
+        }
+        setTimeout(() => textElement.innerText = " ", 2000);
+    };
+};
